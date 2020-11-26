72d5c59675edf8771c45461641f9ae221e36d613
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-11-26 19:59:24 +0900

    matplotlibを使って生成したグラフを吐き出すテスト。

--- src/simulator/views/test.py
+++ src/simulator/views/test.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import io
+
+def plt_to_svg():
+    buf = io.BytesIO()
+    plt.savefig(buf, format='svg', bbox_inches='tight')
+    s = buf.getvalue()
+    buf.close()
+    return s
 
 def versus_test(request):
-    return HttpResponse("Versus Test")
+    x = [0, 1000, 2000, 3000, 4000]
+    y = []
+    z = 1.2
+    for i in x:
+        y.append(i * 10 * z)
+        z = z + 1.5
+    plt.plot(x, y, label="x")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    svg = plt_to_svg()
+    return HttpResponse(svg, content_type='image/svg+xml')
