3ef508dd999719520e9de534248be6246346b947
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2025-08-24 17:58:43 +0900

    基板実装

--- sample.py
+++ sample.py
@@ -4,9 +4,39 @@ class Im6(Machine):
   '''
   天井付きアイム設定６
   '''
+  
+  def __init__(self):
+    super().__init__()
+    self.register_role('BIG', 252, 255),
+    self.register_role('REG', 96, 255),
+    self.register_role('ブドウ', 8, 5.8),
+    self.register_role('リプレイ', 3, 7),
+    self.register_role('チェリー', 2, 33),
+    self.ready()
+
+  def after_play(self, name: str):
+    # 天井
+    if self.games == 499:
+      self.add_medal(10000)
+      self.end_simulate()
+    if name in ['BIG', 'REG']:
+      self.end_simulate()
 
 def main():
   machine = Im6()
+  games = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450]
+  for start in games:
+    num = 1000
+    total = 0
+    total_payout = 0
+    for i in range(num):
+      machine.reset_games(start)
+      while not machine.is_end:
+        machine.play()
+      total += machine.medals
+      total_payout += machine.payout
+      machine.reset()
+    print(f'start: {start}', f'result: {total / num}', f'payout: {total_payout / num}')
   
 if __name__ == '__main__':
   main()
--- simulator/machine.py
+++ simulator/machine.py
@@ -1,4 +1,6 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+import random
+import math
 
 class Machine(ABC):
   '''
@@ -9,4 +11,118 @@ class Machine(ABC):
     '''
     コンストラクタ
     '''
+    self.__roles = []
+    self.__choices = []
+    self.__medals = 0
+    self.__games = 0
+    self.__total_in = 0
+    self.__total_out = 0
+    self.__is_end = False
+
+  def register_role(self, name: str, value: int, rate: float):
+    '''
+    小役登録
+
+    name(str): 小役名
+    value(int): 払い出し枚数
+    rate(float): 小役確率
+    '''
+    self.__roles.append({
+      'name': name,
+      'value': value,
+      'rate': rate,
+    })
+
+  def ready(self):
+    '''
+    準備
+    '''
+    choices = []
+    for role in self.__roles:
+      num = int(65536 / role['rate'])
+      choices.extend([role for i in range(num)])
+    choices += [{'name': '', 'value': 0} for i in range(65536 - len(choices))]
+    self.__choices = choices
+
+  def reset(self):
+    '''
+    リセット
+    '''
+    self.__medals = 0
+    self.__total_in = 0
+    self.__total_out = 0
+    self.__games = 0
+    self.__is_end = False
+
+  def reset_games(self, value: int = 0):
+    '''
+    現在ゲーム数のリセット
+
+    value(int): セットするゲーム数
+    '''
+    self.__games = value
+
+  def end_simulate(self):
+    '''
+    シミュレート終了フラグを立てる
+    '''
+    self.__is_end = True
+
+  def play(self):
+    '''
+    １回転回す
+    '''
+    i = random.randint(0, 65535)
+    r = self.__choices[i]
+    self.__medals += r['value'] - 3
+    self.__games += 1
+    self.__total_in += 3
+    self.__total_out += r['value']
+    self.after_play(r['name'])
+  
+  @abstractmethod
+  def after_play(self, name: str):
+    '''
+    回した後の処理
+    抽象メソッド
+    
+    name(str): 小役名
+    '''
     pass
+
+  def add_medal(self, value: int):
+    '''
+    メダルを強制追加
+
+    value(int): 追加する値
+    '''
+    self.__medals += value
+
+  @property
+  def medals(self):
+    '''
+    差枚
+    '''
+    return self.__medals
+  
+  @property
+  def games(self):
+    '''
+    現在の回転数
+    '''
+    return self.__games
+
+  @property
+  def payout(self):
+    '''
+    機械割
+    '''
+    if self.__total_in == 0: return 100.0
+    return math.floor(self.__total_out / self.__total_in * 1000) / 10
+
+  @property
+  def is_end(self):
+    '''
+    シミュレートが終了しているか
+    '''
+    return self.__is_end
